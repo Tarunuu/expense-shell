eb6bec8dd50b3defa178dcbefc065b9743c08a68
Tarun Budiredla <budiredlatarun@gmail.com>
2025-02-15 17:35:06 -0500

    function

--- backend.sh
+++ backend.sh
@@ -64,7 +64,7 @@ unzip /tmp/backend.zip &>>$LOG_FILE
 VALIDATE $? "Extracting backend application code"
 
 npm install &>>$LOG_FILE
-cp /home/ec2-user/expense-shell/backed.service vim /etc/systemd/system/backend.service
+cp /home/ec2-user/expense-shell/backed.service /etc/systemd/system/backend.service
 
 #load the data before running backend
 
@@ -72,13 +72,13 @@ dnf install mysql -y &>>$LOG_FILE
 VALIDATE $? "Installing MySQL Client"
 
 mysql -h mysql.nobody0.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
-VALIDATE $? "Schems loading"
+VALIDATE $? "Schema loading"
 
 systemctl daemon-reload &>>$LOG_FILE
 VALIDATE $? "Daemon reload"
 
 systemctl enable backend &>>$LOG_FILE
-VALIDATE $? "Enable backend"
+VALIDATE $? "Enabled backend"
 
 systemctl restart backend &>>$LOG_FILE
 VALIDATE $? "Restart Backend"
